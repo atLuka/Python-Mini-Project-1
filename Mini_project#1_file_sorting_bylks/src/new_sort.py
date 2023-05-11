baaf389135195e69a6729dc4a5b20632734d1fa8
Votre Nom <Vous@exemple.com>
2023-05-11 16:46:06 +0200

    version avec bug corrigé + ajout module subprocess

--- Mini_project#1_file_sorting_bylks/src/new_sort.py
+++ Mini_project#1_file_sorting_bylks/src/new_sort.py
@@ -1,59 +1,44 @@
 
 
-from os import *
+import os
+import subprocess 
 from PyQt5 import *
 
 
 
+def new_sort(src_path, tgt_path):
+    files = os.listdir(src_path)
 
-
-
-
-#path="/home/lks/Documents/GIT/fic_cible"
-
-
-def new_sort(src_path,tgt_path):
-
-
-    files=listdir(src_path)
+    w_ext = []
+    wtt_ext = []
     
-    
-    w_ext=[]
-    wtt_ext=[]
+    for i in files:
 
-    for i in files: 
-        if "." in i :
-            w_ext.append(i)
+        if os.path.isfile(os.path.join(src_path, i)): # condition vérifiant que le fichier n'est pas un dossier 
+                if "." in i:
+                    w_ext.append(i)
+                else:
+                    wtt_ext.append(i)
 
-        else:
-            wtt_ext.append(i)
-    
-    
-    if w_ext != []:
-
+    if w_ext: # parcours des fichiers avec une extension
         for i in w_ext:
-            ext=i.rsplit(".",1)[1]
-            
-            if ext not in listdir(tgt_path):
+            ext = i.rsplit(".", 1)[1] # prend l'extension de chaque fichier
 
-                system(f"mkdir {tgt_path}/{ext};mv {src_path}/{i} {tgt_path}/{ext}")
-        
+            if ext not in os.listdir(tgt_path):
+                subprocess.run(["mkdir", f"{tgt_path}/{ext}"])
+                subprocess.run(["mv", f"{src_path}/{i}", f"{tgt_path}/{ext}"])
             else:
-                system(f"mv {src_path}/{i} {tgt_path}/{ext}")
+                subprocess.run(["mv", f"{src_path}/{i}", f"{tgt_path}/{ext}"])
 
-    if wtt_ext != [] : 
-
+    if wtt_ext: # parcours des fichiers sans extension 
         for i in wtt_ext:
-
-            if "autres" not in listdir(tgt_path):
-                system(f"mkdir {tgt_path}/autres ; mv {src_path}/{i} {tgt_path}/autres")
+            if "autres" not in os.listdir(tgt_path):
+                subprocess.run(["mkdir", f"{tgt_path}/autres"])
+                subprocess.run(["mv", f"{src_path}/{i}", f"{tgt_path}/autres"])
             else:
-                system(f"mv {src_path}/{i} {tgt_path}/autres")
-            
+                subprocess.run(["mv", f"{src_path}/{i}", f"{tgt_path}/autres"])
 
 
-
-        
 
 
 
